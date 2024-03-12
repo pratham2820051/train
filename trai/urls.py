f1db0cea10214fd02dfb88765a03caebc9473525
pratham2820051 <prathamph123@gmail.com>
2024-03-12 23:29:56 +0530

    second commit

--- trai/urls.py
+++ trai/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import train_book
 
 urlpatterns = [
-    path('train-book/', views.train_book, name='train_book'),
+    path('', train_book, name='train_book'),  # Remove the extra space in the path pattern
 ]
